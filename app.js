7ed8fcb50d839fca81267f39d33ba30049bf9607
Thuve Rakan <123587624+0xthuve@users.noreply.github.com>
2026-02-21 21:05:26 +0530

    Implement file writing functionality to save 'Hello World!' to file.txt

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Node.js project initialized successfully!");
+const fs = require('fs');
+
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
+});
